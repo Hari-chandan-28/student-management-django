dae772af20c1f3f69e89a5c25be5822e562976fc
Hari chandan <nakka.hari01@gmail.com>
2026-01-03 16:50:28 +0530

    Data added into models

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .import views
+from . import views
 urlpatterns = [
-    path('',views.student_list,name='student_list'),
-    
+    path('', views.student_list, name='student_list'),
+
 ]
