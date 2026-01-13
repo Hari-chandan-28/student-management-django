10ed9c1931dc87f5b59d43d87754d082eb56e22a
Hari chandan <nakka.hari01@gmail.com>
2026-01-13 14:37:21 +0530

    added signup views

--- students/views.py
+++ students/views.py
@@ -3,12 +3,28 @@ from .forms import StudentForm
 from .models import Student
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+def signup(request):
+    if(request.method == 'POST'):
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            user = form.save()
+            login(request,user)
+            return redirect('student_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html',{
+        'form': form
+    })
 
 @login_required
 def student_list(request):
     students = Student.objects.all()
     return render(request , 'students/student_list.html', {'students': students})
 # Create your views here.
+@login_required
 def student_create(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
@@ -20,6 +36,7 @@ def student_create(request):
 
     return render(request, 'students/student_form.html', {'form': form})
 
+@login_required
 def student_update(request, id):
     student = get_object_or_404(Student, id=id)
     form = StudentForm(instance=student)
@@ -35,7 +52,7 @@ def student_update(request, id):
         'students/student_form.html',
         {'form': form, 'is_edit': True}
     )
-
+@login_required
 def student_delete(request ,id):
     student =get_object_or_404(Student ,id=id)
     student.delete()
